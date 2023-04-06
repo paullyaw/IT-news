09309a6d3011496f8201670c03ceb4c4bd1ed122
paullyaw <polina.mitrokhova@gmail.com>
2023-04-07 01:22:11 +0300

    бэйба не перестает работать

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask, render_template, request, redirect, session
+from flask import Flask, render_template, request, redirect, session, flash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SECRET_KEY'] = 'mysecretkey'
-
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
@@ -17,18 +22,40 @@ class User(db.Model):
     role = db.Column(db.String(20), nullable=False)
 
 
+class AccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password',
+                             validators=[EqualTo('confirm_password', message='Passwords must match')])
+    confirm_password = PasswordField('Confirm Password')
+    submit = SubmitField('Save Changes')
+
+
 with app.app_context():
     db.create_all()
 
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 @app.route('/')
 def index():
-    return render_template('base.html')
+    if 'user_id' in session:
+        user = User.query.filter_by(id=session["user_id"]).first()
+        return render_template('home.html', user=user)
+    else:
+        return render_template('base.html')
 
 
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    if current_user.is_authenticated:
+        user = User.query.filter_by(id=current_user.id).first()
+        return render_template('home.html', user=user)
+    else:
+        return redirect('/')
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -53,11 +80,36 @@ def login():
         user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             session['user_id'] = user.id
+            login_user(user)
             return redirect("/home")
         else:
             return render_template('login.html', message='Invalid email or password')
     return render_template('login.html')
 
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect("/")
+
+
+@app.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    form = AccountForm()
+    if request.method == 'POST':
+        user = User.query.filter_by(id=current_user.id).first()
+        user.username = form.username.data
+        user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect('/account')
+    elif request.method == 'GET':
+        user = User.query.filter_by(id=current_user.id).first()
+        form.username.data = user.username
+        form.email.data = user.email
+    return render_template('account.html', form=form)
+
+
 if __name__ == '__main__':
-    app.run(debug=True, port=2000)
+    app.run(debug=True, port=5000)
