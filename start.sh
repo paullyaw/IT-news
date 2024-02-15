976fd7efa915ec2c6396c0de29b39ae2d90385a6
paullyaw <polina.mitrokhova@gmail.com>
2024-02-15 03:43:40 +0300

    !1

--- start.sh
+++ start.sh
@@ -1,11 +1,25 @@
 #!/bin/bash
 
-# Установка виртуального окружения (если необходимо)
-# virtualenv venv
-# source venv/bin/activate
+# Exit early on errors
+set -eu
 
-# Установка зависимостей
-pip install -r requirements.txt
+# Python buffers stdout. Without this, you won't see what you "print" in the Activity Logs
+export PYTHONUNBUFFERED=true
 
-# Запуск Flask-приложения
-python server.py
+# Install Python 3 virtual env
+VIRTUALENV=./venv
+
+if [ ! -d $VIRTUALENV ]; then
+  python3 -m venv $VIRTUALENV
+fi
+
+# Install pip into virtual environment
+if [ ! -f $VIRTUALENV/bin/pip ]; then
+  curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | $VIRTUALENV/bin/python
+fi
+
+# Install the requirements
+$VIRTUALENV/bin/pip install -r requirements.txt
+
+# Run your glorious application
+$VIRTUALENV/bin/python3 server.py
